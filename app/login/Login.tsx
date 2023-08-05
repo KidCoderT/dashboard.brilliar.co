2f2b7c5bc8cd3fbfbfa84b1ef5d28215efcb857a
Tejas <62064506+KidCoderT@users.noreply.github.com>
2023-08-05 23:41:14 +0530

    Cant Login if already logged in

---
+++ app/login/Login.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../database.types";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+const Login = ({ isLoginPage }: { isLoginPage: boolean }) => {
+    const supabase = createClientComponentClient<Database>();
+    const router = useRouter();
+
+    useEffect(() => {
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((state, _) => {
+            if (state === "SIGNED_IN" || state === "SIGNED_OUT")
+                router.push("/");
+        });
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, [router, supabase]);
+
+    return (
+        <div className="min-h-screen w-full py-12 px-6 md:p-24 flex justify-center items-center">
+            <div className="w-full md:w-96 h-fit">
+                <Auth
+                    supabaseClient={supabase}
+                    view={isLoginPage ? "sign_up" : "sign_in"}
+                    appearance={{ theme: ThemeSupa }}
+                    theme="dark"
+                    showLinks={true}
+                    providers={[]}
+                    redirectTo="http://localhost:3000/auth/callback"
+                    socialLayout="horizontal"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,45 +1,26 @@
-"use client";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+    createClientComponentClient,
+    createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { Database } from "../database.types";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Auth } from "@supabase/auth-ui-react";
 import { useEffect } from "react";
 import { redirect, useRouter } from "next/navigation";
+import { cookies } from "next/headers";
+import Login from "./Login";
 
-export default function Page({
+export default async function Page({
     searchParams,
 }: {
     searchParams: { [key: string]: string | string[] | undefined };
 }) {
-    const supabase = createClientComponentClient<Database>();
-    const router = useRouter();
+    const supabase = createServerComponentClient<Database>({ cookies });
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
 
-    useEffect(() => {
-        const {
-            data: { subscription },
-        } = supabase.auth.onAuthStateChange((state, _) => {
-            if (state === "SIGNED_IN") router.push("/");
-        });
+    if (session) redirect("/");
 
-        return () => {
-            subscription.unsubscribe();
-        };
-    }, [router, supabase]);
-
-    return (
-        <div className="min-h-screen w-full py-12 px-6 md:p-24 flex justify-center items-center">
-            <div className="w-full md:w-96 h-fit">
-                <Auth
-                    supabaseClient={supabase}
-                    view={"signup" in searchParams ? "sign_up" : "sign_in"}
-                    appearance={{ theme: ThemeSupa }}
-                    theme="dark"
-                    showLinks={true}
-                    providers={[]}
-                    redirectTo="http://localhost:3000/auth/callback"
-                    socialLayout="horizontal"
-                />
-            </div>
-        </div>
-    );
+    return <Login isLoginPage={"signup" in searchParams} />;
 }
